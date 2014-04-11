2f5e95493fd68e3635d6f97d0e6caad36a1c243b
Robert Paterno <bobpaterno@gmail.com>
2014-04-11 09:08:43 -0500

    added factorial exponent and sqrt

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -8,6 +8,9 @@
     $('#sub').click(sub);
     $('#mul').click(mul);
     $('#div').click(div);
+    $('#exp').click(exp);
+    $('#sqrt').click(sqrt);
+    $('#fact').click(fact);
   }
 
   function add() {
@@ -32,6 +35,34 @@
     var values = getNums();
     var answer = values[0]/values[1];
     display(answer);
+  }
+
+  function exp() {
+    var values = getNums();
+    var answer = Math.pow(values[0], values[1]);
+    display(answer);
+  }
+
+  function sqrt() {
+    var values = getNums();
+    var answer = Math.pow(values[0], 0.5);
+    display(answer);
+  }
+
+  function fact() {
+    var values = getNums();
+    debugger;
+    var answer = calcFact(values[0]);
+    display(answer);
+  }
+
+  function calcFact(num) {
+    var x = num;
+    while(x>1) {
+      num = num*(x-1);
+      x--;
+    }
+    return num;
   }
 
   function getNums() {
